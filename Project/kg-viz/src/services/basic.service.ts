c0ea4e76fcee009e46fab74b3c060f74d0ecbfab
Shriram Ramesh <shriramrseee@gmail.com>
2019-03-29 22:18:55 +0530

    Added project basics

---
+++ Project/kg-viz/src/services/basic.service.ts
@@ -0,0 +1,18 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BasicService {
+
+  url = 'http://10.24.24.214:3000/yagofacts';
+
+  constructor(private http: HttpClient) {
+  }
+
+  getLimitedRows(k : number) {
+    return this.http.get(this.url + "?limit=" + k);
+  }
+
+}
